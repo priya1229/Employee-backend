bb2473c953f7dcea877386ac8587f32b45be89ce
Priya Rajak <priyarajak912@gmail.com>
2024-05-16 01:19:42 +0530

    done

--- api/app.py
+++ api/app.py
@@ -19,7 +19,7 @@ CORS(app, resources={r"/auth/*": {
     "supports_credentials": True
 }})
 
-# MySQL Configuration
+# MySQL Configuration1
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:priya@localhost/emp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
